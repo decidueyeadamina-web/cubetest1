19e93c50dcf13e74890ff8440d7f06a2def80064
decidueyeadamina-web <decidueyeadamina@gmail.com>
2026-09-04 15:04:34 -0700

    Completed Assignment!
    The assignment is done! I just need to do the extra credit...also....I included a Rotate script, a fast rotate script, and a sound script! I used these for 3D prototypes a year ago and am reusing them here with edits! I describe what I know about the scripts in the vscode files and can go in depth about it if necessary

--- Assets/Scripts/RotateFast.cs
+++ Assets/Scripts/RotateFast.cs
@@ -1,13 +1,7 @@
 using UnityEngine;
-using System.Numerics;
 
-public class Rotate : MonoBehaviour
-{   
-    //hi this script is taken from what I've learned in my last coding class (how to rotate things automatically) but altered to mess with turn speed
-    //i didn't use AI i know what all of these things mean at a basic level
-
-    public float turnSpeed = 5;
-
+public class RotateFast : MonoBehaviour
+{
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -17,7 +11,7 @@ public class Rotate : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(new UnityEngine.Vector3(50, 100, 10) * Time.deltaTime); 
+        transform.Rotate(new UnityEngine.Vector3(150, 1000, 100) * Time.deltaTime); 
         //transform.rotate means to make this thing move around its own axis, using vector 3 will have 3 parameters for the x, y, and z access
         //bc my parameters are integers these are static speeds, and Time.deltaTime is the change over time from the last frame
     }
---
+++ Assets/Scripts/playSFX.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class playSFX : MonoBehaviour
+{   
+    //hi again! This is also a script I made in a games tech class a year ago.
+    // I can tell you what these mean generally and I changed things around from what I originally used for this assignment
+
+    public AudioClip breakSound;
+    private AudioSource groundAudio;
+
+    //these are some parts I actually don't get a lot! I know Audio clip is what I use as a place on the script to link the .wav file
+    // AudioSource is how I attach that component to this script
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //at first, the sounds had to be automatic so everything was placed under update, which I changed here.
+    }
+
+    void OnTriggerEnter (Collider other)
+    {
+        if(other.gameObject.CompareTag("Ground"))
+        {
+            groundAudio = GetComponent<AudioSource>();
+            groundAudio.PlayOneShot(breakSound);
+        } 
+
+        // ok so! the scripts inside the if/then statement call on the audio to be played
+        //the ontriggerenter is for when 2 colliders make contact (the parameters are specifically for 3D)
+        //line 28 is specifically about which tag the game object that the script is attached to has to make contact with for the inner script to trigger
+    }
+}
